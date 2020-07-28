fce3b7cb6086194353dbf6f11629350ac17c2ab8
Darbaz Ali <darbaz.a.h@gmail.com>
2020-07-28 15:13:14 +0300

    plot cells

--- src/App.js
+++ src/App.js
@@ -4,6 +4,20 @@ import * as d3 from 'd3';
 // target the container node, this will be the frame for the entier application
 const container = d3.select("#container")
 const baseTemp = 8.66;
+const months = [
+  "January",
+  "February",
+  "March",
+  "April",
+  "May",
+  'June',
+  'July',
+  'Augast',
+  'September',
+  'November',
+  'October',
+  'Desember'
+]
 // create a title for the app
 container
   .append('h2')
@@ -55,6 +69,13 @@ const yScale = d3
   
 
 const parseTimeYear = d3.timeParse("%Y");
+const parseTimeMonth = d3.timeParse("%m")
+
+const fillCell = temp => {
+  if ( temp < 3 ) {
+    return ""
+  }
+}
 
 /*============================================== 
   CHART DRAWER FUNCTION
@@ -64,12 +85,15 @@ const drawHeatMap = data => {
   // format data
   data.forEach(d => {
     d["year"] = parseTimeYear(d["year"])
+    d["month"] = d["month"] - 1;
+    d.temp = Number.parseFloat(baseTemp + d.variance).toFixed(1);
   });
 
-  // create domains
+  /*============================================== 
+  X Axis
+  ===============================================*/
   xScale
     .domain(d3.extent( data, d => d["year"]))
-
 
   // create Axes
   const xAxis = d3
@@ -82,8 +106,57 @@ const drawHeatMap = data => {
     .attr('transform', `translate(0, ${height})`)
     .call(xAxis)
 
+    /*============================================== 
+    Y Axis
+    ===============================================*/
+    yScale
+      .domain(d3.extent( data, d => d["month"]))
+    
+    const yAxis = d3
+      .axisLeft(yScale)
 
+
+      
+
+    svgGroups
+      .append('g')
+      .attr('id', 'y-axis')
+      .call(yAxis)
+
+
+     /*============================================== 
+      PLOT CELLS
+    ===============================================*/
+    svgGroups
+      .selectAll('rect')
+      .data(data)
+      .enter()
+      .append('rect')
+
+      // data attributes
+      .attr('data-month', d => d.month)
+      .attr('data-year', d => d.year.getFullYear())
+      .attr('data-temp', d => d.temp)
+
+
+      // set coordinates
+      .attr('x', d => xScale(d.year))
+      .attr('y', d => d.month * 30)
+
+      // set size
+      .attr('width', 2)
+      .attr('height', (height / 12))
+
+      .attr('class', 'cell')
 }
+
+
+/* 
+
+  "year": 1754,
+  "month": 2,
+  "variance": -4.175
+*/
 
 /*============================================== 
   GRAB DATA WITH FETCH API
@@ -92,6 +165,6 @@ const api_url = "https://raw.githubusercontent.com/freeCodeCamp/ProjectReference
 fetch( api_url )
 .then( res => res.json())
 .then( json => {
-  const data = json["monthlyVariance"]
+  const data = json.monthlyVariance
   drawHeatMap(data)
 })
