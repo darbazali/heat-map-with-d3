4c824ee17ff4d899a2a5e44e11a78e2e945e6831
Darbaz Ali <darbaz.a.h@gmail.com>
2020-07-28 09:26:31 +0300

    create canvas and groups

--- src/App.js
+++ src/App.js
@@ -1,3 +1,42 @@
+// import d3 package
+import * as d3 from 'd3';
+import { transition } from 'd3';
+
+// target the container node, this will be the frame for the entier application
+const container = d3.select("#container")
+
+/*============================================== 
+  DEFINE THE ATTRIBURES
+===============================================*/
+
+// define margins
+const margin = {
+  top: 20,
+  right: 20,
+  bottom: 20,
+  left: 60
+}
+
+// define dimentions for the chart
+const width = 800 - margin.left - margin.right;
+const height = 400 - margin.top - margin.bottom;
+
+// crate the svg canvas
+const canvas = container
+  .append('svg')
+  .attr('viewBox', `0 0 ${width + margin.right + margin.left} ${height + margin.top + margin.bottom}`)
+
+// create a group for the svg elements
+const svgGroups = canvas
+  .append('g')
+  .attr('transform', `translate(${margin.left}, ${margin.top})`)
+
+
+// create a title for the app
+container
+  .append('h2')
+  .attr('id', 'title')
+  .text("Monthly Global Land-Surface Temperature")
 
 
 
